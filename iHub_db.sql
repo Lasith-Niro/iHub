5902792bd730739a3fc14fe568b3411aaef01fbd
Lasith-Niro <lasithniro.online@gmail.com>
2015-09-05 22:25:36 +0530

    update db
    @lasi

--- iHub_db.sql
+++ iHub_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 05, 2015 at 01:05 ප.ව.
+-- Generation Time: Sep 05, 2015 at 06:55 ප.ව.
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -23,26 +23,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `group`
---
-
-CREATE TABLE IF NOT EXISTS `group` (
-  `grpName` varchar(20) NOT NULL,
-  `gID` int(5) NOT NULL AUTO_INCREMENT,
-  `AdminID` int(5) NOT NULL,
-  `ModeratorID` int(5) NOT NULL,
-  PRIMARY KEY (`gID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `group_users`
 --
 
 CREATE TABLE IF NOT EXISTS `group_users` (
-  `gID` int(5) NOT NULL,
-  `userID` int(5) NOT NULL
+  `gID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -70,6 +56,29 @@ INSERT INTO `levels` (`id`, `name`, `permissions`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `userGroups`
+--
+
+CREATE TABLE IF NOT EXISTS `userGroups` (
+  `gID` int(11) NOT NULL AUTO_INCREMENT,
+  `grpName` varchar(50) NOT NULL,
+  `AdminID` int(11) NOT NULL,
+  `ModeratorID` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  PRIMARY KEY (`gID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `userGroups`
+--
+
+INSERT INTO `userGroups` (`gID`, `grpName`, `AdminID`, `ModeratorID`, `date`, `time`) VALUES
+(1, 'lasithGroup', 1, 1, '2015-09-05', '06:47:56');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -84,15 +93,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `levels` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `fname`, `lname`, `email`, `phone`, `levels`) VALUES
-(1, 'Lasith', '3d1b33ad0aee33adbf451ca2104a2c3b63757943c6d85b45ceb0c00662c7cf0f', 'lasith123', 'niroshan', 'lasith2013.l2n@gmail.org', '0712837662', 1),
-(2, 'Sunimal', '3d1b33ad0aee33adbf451ca2104a2c3b63757943c6d85b45ceb0c00662c7cf0f', 'sunimal', 'malkakulage', 'sskmal@gmail.com', '0712837555', 1);
+(1, 'Lasith', '3d1b33ad0aee33adbf451ca2104a2c3b63757943c6d85b45ceb0c00662c7cf0f', 'lasith123', 'niroshan', 'lasith2013.l2n@gmail.org', '0712837662', 3),
+(2, 'Sunimal', '3d1b33ad0aee33adbf451ca2104a2c3b63757943c6d85b45ceb0c00662c7cf0f', 'sunimal', 'malkakulage', 'sskmal@gmail.com', '0712837555', 1),
+(3, 'NewTest', 'f2d30afea3131fcffa86fc0c76cbc5f1f0e5940c587af3a37a4df46c7a89c72a', 'new', 'user', 'lasith2013.l2n@gmail.com', '0712837662', 1);
 
 -- --------------------------------------------------------
 
